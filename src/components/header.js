aafe0b9334872fadf7d15437a49b3d19a8e3b3b2
Joshtray <jesseyuchenwichi@gmail.com>
2021-09-07 13:13:00 +0900

    Included header component and pages

---
+++ src/components/header.js
@@ -0,0 +1,18 @@
+import React from "react";
+import './header.css';
+
+export default function Header () {
+    return (
+        <div className="header">
+            <div className="logo">
+                LOGO
+            </div>
+            <div className="search-bar">
+                SEARCH BAR
+            </div>
+            <div className="cart">
+                CART
+            </div>
+        </div>
+    )
+}
