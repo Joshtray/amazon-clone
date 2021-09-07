a93653aef778f4dc5edbea128faa0002c0ad7271
Joshtray <jesseyuchenwichi@gmail.com>
2021-09-07 13:52:19 +0900

    added navigation for the header

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 import Header from './components/header';
 import './App.css';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Home from './components/pages/Home';
+import Cart from './components/pages/Cart';
 
 function App() {
   return (
     <div className="App">
-      <Header />
+      <Router>
+        <Header />
+        <Switch>
+          <Route path="/" exact component={Home} />
+          <Route path="/cart" exact component={Cart} />
+        </Switch>
+        </Router>
     </div>
   );
 }
---
+++ src/components/pages/Cart.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Cart.css'
+
+const Cart = () => {
+    return (
+        <div>
+            CART
+        </div>
+    )
+}
+
+export default Cart
