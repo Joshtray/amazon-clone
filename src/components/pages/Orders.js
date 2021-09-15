65058553f8d3aec49087400c3431cc0734ea7a65
Joshtray <jesseyuchenwichi@gmail.com>
2021-09-15 17:08:02 +0900

    Added product creation, update and display features

--- src/components/pages/Orders.js
+++ src/components/pages/Orders.js
@@ -1,13 +1,22 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import './Orders.css'
 import Product from './Product'
 import Item from '../../data/Item'
+import { API, graphqlOperation }from 'aws-amplify'
+import { listProducts } from '../../graphql/queries'
 
 const Orders = () => {
+    const [product, setProduct] = useState([])
+    const fetchItems = async () => {
+        const itemList = await API.graphql(graphqlOperation(listProducts))
+        setProduct(itemList.data.listProducts.items)
+    }
+    useEffect(() => {
+        fetchItems()
+    }, [])
     return (
         <div>
-            {Item.map((item) => (<Product product={item} />))}
-            
+            {product.map((item) => (<Product product={item} />))} 
         </div>
     )
 }
