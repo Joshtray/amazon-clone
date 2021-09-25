35f3ec7f7ca8ec7b1a09bc938f56accc2f77e2eb
Tayomide <tibrahim@hawk.iit.edu>
2021-09-24 22:13:30 -0500

    Add images and style 404 page

--- src/components/pages/NotFound.js
+++ src/components/pages/NotFound.js
@@ -1,13 +1,43 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import styled from 'styled-components'
 
 const NotFound = () => {
     return (
-        <div>
+        <Wrap>
             <h1>404 Not Found</h1>
-            <Link to="/">Return to Home</Link>
-        </div>
+            <Cutehold>
+              <img src="https://www.pixelstalk.net/wp-content/uploads/2016/03/Cute-animal-wallpapers-hd-desktop.jpg" alt="Missed cute picture" />
+            </Cutehold>
+        </Wrap>
     )
 }
+
+const Wrap = styled.div`
+  margin: 10px auto auto auto;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+  height: calc(100vh - 110px);
+  width: calc(100vh - 160px);
+  h1{
+    margin: 0px;
+    font-size: 25px;
+  }
+
+`
+
+const Cutehold = styled.div`
+  flex: 1;
+  margin-top: 25px;
+  overflow: hidden;
+  border-radius: 50%;
+  img{
+    height: 100%;
+    width: 100%;
+    object-fit: cover;
+  }
+`
 
 export default NotFound
