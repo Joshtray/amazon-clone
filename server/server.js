9cd921ba8d30c7328d81f7368e610d7e64286439
Joshtray <jesseyuchenwichi@gmail.com>
2021-10-12 06:04:58 +0900

    recommit

--- server/server.js
+++ server/server.js
@@ -38,7 +38,7 @@ app.get('/', (req, res) => {
   console.log(process.cwd())
   console.log(process.env.STATIC_DIR)
   console.log(__dirname)
-  res.sendFile("../client/build/index.html")
+  res.sendFile(path.resolve(__dirname, '../client/build', 'index.html'));
 })
 
 // app.get('*', (req, res) => {
