877cac16978e80b3cefdbff26241033e36c0e679
Tayomide <tibrahim@hawk.iit.edu>
2021-10-11 19:06:48 -0500

    Add changes from github

---
+++ src/data/Item.js
@@ -0,0 +1,81 @@
+export default [{
+        id: '1',
+        name: 'High quality blender',
+        imageUrl: 'https://b3h2.scene7.com/is/image/BedBathandBeyond/2020-05-08-10-54_622356561884_imageset?$690$&wid=690&hei=690',
+        description: 'A high quality blender for kitchenuse and cooking',
+        price: 50.00,
+        ratings: [
+            {
+                id: 5,
+                user: {
+                    name: 'Joshtray'
+                },
+                rating: 5.0,
+                description: 'Nice product! Works perfectly!'
+            },
+            {
+                id: 8,
+                user: {
+                    name: 'Tomiwa'
+                },
+                rating: 4.0,
+                description: 'Very good product. A bit hard to understand how to use at first but it\'s fine'
+            }
+        ],
+        types: [
+            {
+                name: 'Black',
+                imageUrl: 'https://b3h2.scene7.com/is/image/BedBathandBeyond/2020-05-08-10-54_622356561884_imageset?$690$&wid=690&hei=690',
+                price: 50.00
+            },
+            {
+                name: 'White',
+                imageUrl: 'https://cdn-static-toshiba.midea.com/indonesia/productGallery/ID-PP-BL-BL-15PH2.jpg?t=1618440719012',
+                price: 50.00
+            }
+        ],
+        deals: [],
+        quantity: 10,
+        category: 'Kitchen'
+    },
+    {
+        id: '2',
+        name: 'Microsoft Surface Laptop',
+        imageUrl: 'https://www.notebookcheck.biz/uploads/tx_nbc2/MicrosoftSurfaceLaptop3-15__1_.JPG',
+        description: 'Microsoft Surface Laptop for official use as well as entertainment',
+        price: 1000.00,
+        ratings: [
+            {
+                id: 5,
+                user: {
+                    name: 'Joshtray'
+                },
+                rating: 4.0,
+                description: 'Nice product! A bit slower than I expected though'
+            },
+            {
+                id: 8,
+                user: {
+                    name: 'Tomiwa'
+                },
+                rating: 2.0,
+                description: 'Very slow, low storage space, don\'t reccommend'
+            }
+        ],
+        types: [
+            {
+                name: 'Silver',
+                imageUrl: 'https://www.notebookcheck.biz/uploads/tx_nbc2/MicrosoftSurfaceLaptop3-15__1_.JPG',
+                price: 1000.00
+            },
+            {
+                name: 'Black',
+                imageUrl: 'https://m.media-amazon.com/images/I/71g3yqyKNyL._AC_SL1500_.jpg',
+                price: 1000.00
+            }
+        ],
+        deals: [],
+        quantity: 20,
+        category: 'Office'
+    }
+]
